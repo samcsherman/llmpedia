a2bdc5182acc1a1e46e6305d1df9b31c51a0a02d
Sam Sherman <samuel.sherman@projectcanary.com>
2023-10-12 17:00:07 -0600

    stuff

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# if "papers" not in st.session_state:
-#     st.session_state.papers = None
-#
-# if "page_number" not in st.session_state:
-#     st.session_state.page_number = 0
-#
-# if "num_pages" not in st.session_state:
-#     st.session_state.num_pages = 0
-#
+if "papers" not in st.session_state:
+    st.session_state.papers = None
+
+if "page_number" not in st.session_state:
+    st.session_state.page_number = 0
+
+if "num_pages" not in st.session_state:
+    st.session_state.num_pages = 0
+
 if "arxiv_code" not in st.session_state:
     st.session_state.arxiv_code = ""
 
@@ -246,7 +246,7 @@ def main():
     ## Sort by.
     sort_by = st.sidebar.selectbox(
         "Sort By",
-        ["Sentiment Score Ascending", "Sentiment Score Descending" "Company"],
+        ["Sentiment Score Ascending", "Sentiment Score Descending", "Company"],
     )
 
     ## Company Filter.
